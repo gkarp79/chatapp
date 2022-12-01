960ea3f3c6b2e4330dac8aa820d4e1b01a836acc
gkarp79 <58459055+gkarp79@users.noreply.github.com>
2022-12-01 15:05:49 +0900

    Initial commit

---
+++ src/main/java/com/cors/chatapp/ChatController.java
@@ -0,0 +1,38 @@
+package com.cors.chatapp;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+    private final chatRepository chatRepository;
+
+
+    // 귓속말 할때 사용
+    @CrossOrigin
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
+        return chatRepository.mFindBySender(sender, receiver).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable Integer roomNum){
+        return chatRepository.mFindByRoomNum(roomNum).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @CrossOrigin
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); //Object를 리턴하면 자동으로 JSON 변환(MessageConverter)
+    }
+}
